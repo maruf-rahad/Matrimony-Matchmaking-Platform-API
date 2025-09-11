af642670c2ed96d05806a6a96b44f64830971269
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2025-09-11 09:42:13 +0200

    added city and country field

--- src/main/java/com/eu/demomatrimony/dto/ProfileDto.java
+++ src/main/java/com/eu/demomatrimony/dto/ProfileDto.java
@@ -23,6 +23,24 @@ public class ProfileDto {
     private String fatherOccupation;
     private String motherOccupation;
     private String numberOfSiblings;
+    private String city;
+    private String country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public String getName() {
         return name;
--- src/main/java/com/eu/demomatrimony/models/Profile.java
+++ src/main/java/com/eu/demomatrimony/models/Profile.java
@@ -38,6 +38,24 @@ public class Profile extends BaseModel{
     private String fatherOccupation;
     private String motherOccupation;
     private String numberOfSiblings;
+    private String city;
+    private String country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public Long getId() {
         return id;
